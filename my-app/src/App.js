3170305a73b6bf476dac98906e753f6e564dc773
kevincakey <57571360+kevincakey@users.noreply.github.com>
2024-04-06 17:50:26 -0700

    Brainstorming website layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p></p>
+        <p>Home</p>
+        <p>Projects</p>
+        <p>Contact Me!</p>
       </header>
     </div>
   );
