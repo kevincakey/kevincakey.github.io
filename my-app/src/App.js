6cfb7c2e716bd73ac1cb189317b6b0597582aca5
kevincakey <57571360+kevincakey@users.noreply.github.com>
2024-04-09 03:53:54 -0700

    Moved NavBar to be its own a separate component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,12 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/About">About</Link>
-            </li>
-            <li>
-              <Link to="/Projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/About" element={<About></About>}></Route>
-          <Route path="/Projects" element={<Projects></Projects>}></Route>
-          <Route path="/Contact" element={<Contact></Contact>}></Route>
-        </Routes>
+        <Navbar></Navbar>
       </header>
     </div>
   );
--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
+import { Link, Route, Routes } from "react-router-dom";
 
 export default function Navbar(){
 	return (
 		<div>
-			<ul>
-				<li>Home</li>
-				<li>Projects</li>
-				<li>Contact Me!</li>
-			</ul>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/About">About</Link>
+					</li>
+					<li>
+						<Link to="/Projects">Projects</Link>
+					</li>
+					<li>
+						<Link to="/Contact">Contact</Link>
+					</li>
+				</ul>
+			</nav>
+			<Routes>
+				<Route path="/" element={<Home></Home>}></Route>
+				<Route path="/About" element={<About></About>}></Route>
+				<Route path="/Projects" element={<Projects></Projects>}></Route>
+				<Route path="/Contact" element={<Contact></Contact>}></Route>
+			</Routes>
 		</div>
 	);
 }
