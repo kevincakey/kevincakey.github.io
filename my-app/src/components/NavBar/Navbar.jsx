5c9a39d65d902d43864f4a1b1b621e4502c32c6b
kevincakey <57571360+kevincakey@users.noreply.github.com>
2024-04-11 05:08:05 -0700

    Added in states to navBar

--- my-app/src/components/NavBar/Navbar.jsx
+++ my-app/src/components/NavBar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.scss";
 import { Link} from "react-router-dom";
 import homeIcon from "../../assets/homeIcon.json";
@@ -8,21 +8,28 @@ import contactIcon from "../../assets/contactIcon.json";
 import Lottie from 'lottie-react';
 
 const Navbar = () => {
+
+	const [hoverHome, setHoverHome] = useState(false);
+	const [hoverAbout, setHoverAbout] = useState(false);
+	const [hoverProjects, setHoverProjects] = useState(false);
+	const [hoverContact, setHoverContact] = useState(false);
+	
+
 	return (
 		<div className="navbar">
 			<nav>
 				<ul>
 					<li>
-						<Link to="/"><Lottie animationData={homeIcon}/></Link>
+						<Link to="/"><Lottie animationData={homeIcon} loop={hoverHome}/></Link>
 					</li>
 					<li>
-						<Link to="/About"><Lottie animationData={aboutIcon}/></Link>
+						<Link to="/About"><Lottie animationData={aboutIcon} loop={hoverAbout}/></Link>
 					</li>
 					<li>
-						<Link to="/Projects"><Lottie animationData={projectsIcon}/></Link>
+						<Link to="/Projects"><Lottie animationData={projectsIcon} loop={hoverProjects}/></Link>
 					</li>
 					<li>
-						<Link to="/Contact"><Lottie animationData={contactIcon}/></Link>
+						<Link to="/Contact"><Lottie animationData={contactIcon} loop={hoverContact}/></Link>
 					</li>
 				</ul>
 			</nav>
