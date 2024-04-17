2a337d5e229e66117a4e23ecd469504fd301ce1f
kevincakey <57571360+kevincakey@users.noreply.github.com>
2024-04-17 02:47:27 -0400

    Added Cake icon and updated map skin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./Home.scss"
+import { DotLottiePlayer } from '@dotlottie/react-player';
+import '@dotlottie/react-player/dist/index.css';
+import cakeAnimation from "../assets/icons/cakeAnimation.lottie";
 
 const Home = () => {
   return (
-	<h1>Home</h1>
+    <><h1>Home</h1>
+    <DotLottiePlayer
+							src={cakeAnimation}
+              autoplay
+							loop
+              speed = "2"
+							background="transparent"
+							>
+							</DotLottiePlayer></>
   )
 }
 
