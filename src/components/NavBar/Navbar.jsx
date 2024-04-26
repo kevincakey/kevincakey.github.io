50c40c7329942dc2709a076bbcb47e44c4876805
kevincakey <57571360+kevincakey@users.noreply.github.com>
2024-04-26 00:58:32 -0700

    started on framer, home animation done

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,88 +1,105 @@
-import React from 'react';
+import React from "react";
 import "./Navbar.scss";
-import { Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import homeIcon from "../../assets/icons/homeIcon.lottie";
 import aboutIcon from "../../assets/icons/aboutIcon.lottie";
 import projectsIcon from "../../assets/icons/projectsIcon.lottie";
 import contactIcon from "../../assets/icons/contactIcon.lottie";
-import { DotLottiePlayer } from '@dotlottie/react-player';
-import '@dotlottie/react-player/dist/index.css';
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import "@dotlottie/react-player/dist/index.css";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-	const location = useLocation();
-	const currentPath = location.pathname;
+  const location = useLocation();
+  const currentPath = location.pathname;
 
-	let navbarClass = "navbar-container";
+  let navbarClass = "navbar-container";
 
-    // Add additional conditions as needed for different routes
-    if (currentPath === "/") {
-        navbarClass += " home-route";
-    } else if (currentPath === "/About") {
-        navbarClass += " about-route";
-    } else if (currentPath === "/Projects") {
-        navbarClass += " projects-route";
-    } else if (currentPath === "/Contact") {
-        navbarClass += " contact-route";
-    }
+  // Add additional conditions as needed for different routes
+  if (currentPath === "/") {
+    navbarClass += " home-route";
+  } else if (currentPath === "/About") {
+    navbarClass += " about-route";
+  } else if (currentPath === "/Projects") {
+    navbarClass += " projects-route";
+  } else if (currentPath === "/Contact") {
+    navbarClass += " contact-route";
+  }
 
-	return (
-		<div className={navbarClass}>
-			<div id="topCake"></div>
-			<nav>
-				<ul id="navbar-icons">
-					<li>
-						<Link to="/">
-							<DotLottiePlayer
-							src={homeIcon}
-							loop
-							hover
-							background="transparent"
-							>
-							</DotLottiePlayer>
-						</Link>
-					</li>
-					<li>
-						<Link to="/About">
-							<DotLottiePlayer
-							src={aboutIcon}
-							loop
-							hover
-							background="transparent"
-							>
-							</DotLottiePlayer>
-							</Link>
-					</li>
-					<li>
-						<Link to="/Projects">
-							<div className="icon-background">
-								<DotLottiePlayer
-								src={projectsIcon}
-								loop
-								hover
-								background="transparent"
-								>
-								</DotLottiePlayer>
-							</div>
-						</Link>
-					</li>
-					<li>
-						<Link to="/Contact">
-							<div className="icon-background">
-								<DotLottiePlayer
-								src={contactIcon}
-								loop
-								hover
-								background="transparent"
-								>
-								</DotLottiePlayer>
-							</div>
-						</Link>
-					</li>
-				</ul>
-			</nav>
-			<div id="cakeBase"></div>
-		</div>
-	);
-}
+  return (
+    <div className={navbarClass}>
+      <div id="topCake"></div>
+      <motion.nav
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 3 }}
+      >
+        <motion.ul id="navbar-icons">
+          <motion.li
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", delay: 0.2, stiffness: 20 }}
+          >
+            <Link to="/">
+              <DotLottiePlayer
+                src={homeIcon}
+                loop
+                hover
+                background="transparent"
+              ></DotLottiePlayer>
+            </Link>
+          </motion.li>
+          <motion.li
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", delay: 0.4, stiffness: 20 }}
+          >
+            <Link to="/About">
+              <DotLottiePlayer
+                src={aboutIcon}
+                loop
+                hover
+                background="transparent"
+              ></DotLottiePlayer>
+            </Link>
+          </motion.li>
+          <motion.li
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", delay: 0.6, stiffness: 20 }}
+          >
+            <Link to="/Projects">
+              <div className="icon-background">
+                <DotLottiePlayer
+                  src={projectsIcon}
+                  loop
+                  hover
+                  background="transparent"
+                ></DotLottiePlayer>
+              </div>
+            </Link>
+          </motion.li>
+          <motion.li
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", delay: 0.8, stiffness: 20 }}
+          >
+            <Link to="/Contact">
+              <div className="icon-background">
+                <DotLottiePlayer
+                  src={contactIcon}
+                  loop
+                  hover
+                  background="transparent"
+                ></DotLottiePlayer>
+              </div>
+            </Link>
+          </motion.li>
+        </motion.ul>
+      </motion.nav>
+      <div id="cakeBase"></div>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,17 +3,25 @@ import "./Home.scss";
 import { DotLottiePlayer } from "@dotlottie/react-player";
 import "@dotlottie/react-player/dist/index.css";
 import cakeAnimation from "../../assets/icons/cakeAnimation.lottie";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div id="HomeContainer">
-      <div className="lottie-container">
-        <DotLottiePlayer
-          src={cakeAnimation}
-          autoplay
-          speed="2"
-          background="transparent"
-        ></DotLottiePlayer>
+    <div id="cover">
+      <div id="HomeContainer">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 5 }}
+          className="lottie-container"
+        >
+          <DotLottiePlayer
+            src={cakeAnimation}
+            autoplay
+            speed="2"
+            background="transparent"
+          ></DotLottiePlayer>
+        </motion.div>
       </div>
     </div>
   );
