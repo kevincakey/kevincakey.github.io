95d7e857931d19a155c47bca43aa895a7d81a14e
kevincakey <57571360+kevincakey@users.noreply.github.com>
2024-04-30 03:00:42 -0700

    banner touch ups

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -95,6 +95,8 @@ const Banner = () => {
               className="animatedBannerText"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 2 }}
             >
               {phrases[phraseIndex]}
             </motion.p>
