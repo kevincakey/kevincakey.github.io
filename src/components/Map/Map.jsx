d63b6aff1bcba635876c42c39b83b96005002a2a
kevincakey <57571360+kevincakey@users.noreply.github.com>
2025-06-23 07:02:48 -0700

    leaflet fixed

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import "./Map.scss";
-import { MapContainer } from "react-leaflet/MapContainer";
-import { TileLayer } from "react-leaflet/TileLayer";
-import { Marker } from "react-leaflet/Marker";
-import { Popup } from "react-leaflet/Popup";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import markerShadowPng from "leaflet/dist/images/marker-shadow.png";
 // import "leaflet/dist/leaflet.css"
+
+const customIcon = new L.Icon({
+  iconUrl: markerIconPng,
+  shadowUrl: markerShadowPng,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 const Map = () => {
   return (
     <div id="map">
       <MapContainer center={[37.56, -122.05]} zoom={7}>
-        {/* <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-        <TileLayer
-          attribution='<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>'
-          url="https://wxs.ign.fr/choisirgeoportail/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/png&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}.png"
-        />
-        <Marker position={[37.56, -122.05]}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[37.56, -122.05]} icon={customIcon}>
           <Popup>
             <h2>Here I am</h2>
           </Popup>
